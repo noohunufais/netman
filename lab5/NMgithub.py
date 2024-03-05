eb039578011d5c7a4679901fdc07c45310c679d4
noohunufais <noohunufais.s@gmail.com>
2024-03-05 15:58:38 -0700

    Changes detected and pushing to GitHub via NMgithub.py

--- lab5/NMgithub.py
+++ lab5/NMgithub.py
@@ -1,12 +1,9 @@
 import git
 
-try:
-    rep_path = "/home/student/git/netman"
-    repo = git.Repo(rep_path)
-    git = git.Git(rep_path)
-    repo.git.add(update=True)
-    repo.index.commit("Changes detected and pushing to GitHub via NMgithub.py")
-    origin = repo.remote('origin')
-    origin.push()
-except Exception as e:
-    print(e)
+rep_path = "/home/student/git/netman"
+repo = git.Repo(rep_path)
+git = git.Git(rep_path)
+repo.git.add(update=True)
+repo.index.commit("Changes detected and pushing to GitHub via NMgithub.py")
+origin = repo.remote('origin')
+origin.push()
